773353fd62dc1e753f776d5ec09fc4542e85f39b
Sebastian Skałacki <skalee@gmail.com>
2018-03-13 11:35:44 +0100

    Initial commit

---
+++ lib/country_to_locales_mapping.rb
@@ -0,0 +1,5 @@
+require "country_to_locales_mapping/version"
+
+module CountryToLocalesMapping
+  # Your code goes here...
+end
---
+++ lib/country_to_locales_mapping/version.rb
@@ -0,0 +1,3 @@
+module CountryToLocalesMapping
+  VERSION = "0.1.0"
+end
